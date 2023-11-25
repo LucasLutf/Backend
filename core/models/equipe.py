7f71c3df48371c2644342af63ecc5c5c07c50212
viniihds <viniciushsilva2210@gmail.com>
2023-11-25 01:37:38 -0300

    Correção models Participante e Equipe

--- core/models/equipe.py
+++ core/models/equipe.py
@@ -1,6 +1,7 @@
 from django.db import models
 from uploader.models import Image
 from core.models.evento import Evento
+from core.models.participante import Participante
 
 
 class Equipe(models.Model):
@@ -15,7 +16,7 @@ class Equipe(models.Model):
         default=None,
     )
     evento = models.ForeignKey(Evento, on_delete=models.PROTECT, related_name="equipes")
-
+    membros = models.ManyToManyField(Participante, related_name="equipes")
 
     def __str__(self):
         return self.nome
